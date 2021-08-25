119c25a563214cd4620be0282d732ca9f7fa8616
Rasila Hirani <darshanhirani@rasilas-MacBook-Air.local>
2021-08-25 16:03:15 +0100

    finished styling for menu-item

--- src/component/homepage.jsx
+++ src/component/homepage.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import '../styles/component/homepage.scss'
+import Directory  from './directory';
 const HomePage =()=>(
     <div className="homepage">
-        <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">HATS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-        </div>
+        <Directory/>
     </div>
 );
 
