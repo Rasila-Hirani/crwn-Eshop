24f5079b66c965144d646cd08e2a82490c099912
Rasila Hirani <darshanhirani@rasilas-MacBook-Air.local>
2021-09-07 11:59:44 +0100

    add sigin form with sign in with google

--- src/App.js
+++ src/App.js
@@ -4,17 +4,43 @@ import './App.css';
 import HomePage from './component/pages/homepage/homepage';
 import Header from './component/header/header';
 import ShopPage from './component/pages/shop/shop';
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route path='/' component={HomePage} exact/>
-        <Route path='/shop' component={ShopPage} />
-      </Switch>
-     
-    </div>
-  );
+import {auth} from './firebase/firebase.utils';
+import SignInSignUpPage from './component/pages/sigin-in-and-sign-up/sigin-in-and-sign-up';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    
+    this.state={
+      currentUser:null
+    }
+  }
+  unsubscriveFromAuth=null;
+
+  componentDidMount(){
+  this.unsubscriveFromAuth = auth.onAuthStateChanged(user =>{
+      this.setState({currentUser:user});
+      console.log(user )
+    })
+  }
+  componentWillUnmount(){
+    this.unsubscriveFromAuth();
+  }
+  render(){
+    return (
+      <div>
+        
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route path='/' component={HomePage} exact/>
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInSignUpPage}/>
+        </Switch>
+       
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/component/pages/sigin-in-and-sign-up/sigin-in-and-sign-up.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sigin-in-and-sign-up.scss';
+import SignIn from '../../sign-in/sign-in';
+
+const SignInSignUpPage =() =>(
+    <div className="sign-in-and-sign-up">
+        <SignIn/>
+
+    </div>
+
+);
+export default SignInSignUpPage;
