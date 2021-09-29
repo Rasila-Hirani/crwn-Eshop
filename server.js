c782f1d4598155fd37f705a3ab102b662c3b266b
Rasila Hirani <darshanhirani@rasilas-MacBook-Air.local>
2021-09-29 13:31:38 +0100

    update package.json file

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
 require("dotenv").config();
-
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-
-const app = express();
-
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
 app.use(cors());
 
 console.log('process.env.NODE_ENV',process.env.NODE_ENV)
+
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,'client/build')))
     app.get('*',function(req,res){
@@ -35,6 +32,7 @@ app.post('/payment',(req,res) =>{
         amount:req.body.amount,
         currency:'usd'
     };
+    
     stripe.charges.create(body,(stripeErr,stripeRes)=>{
         if(stripeErr){
             res.status(500).send({error:stripeErr})
